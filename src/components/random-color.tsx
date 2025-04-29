5bb6b8eb42eb074b944ade3246b64e9b0cc89d7e
nivance <limingjian110@163.com>
2025-04-29 16:56:13 +0800

    add copy codes

--- src/components/random-color.tsx
+++ src/components/random-color.tsx
@@ -12,21 +12,32 @@ export default function RandomColor() {
         setRandomColors(colors);
     }, []);
 
-    console.log("randomColors length: " + randomColors.length);
     return (
         <section className="mb-12">
             <h1 className="text-2xl font-bold text-white text-center mb-4">1000 Random Color</h1>
             <div className="mx-auto w-[80%]">
-                <div className="grid grid-cols-3 md:grid-cols-3 lg:grid-cols-5 gap-4" style={{ rowGap: '2px' }}>
+                <div className="grid grid-cols-3 md:grid-cols-3 lg:grid-cols-5 gap-4" style={{ rowGap: '4px' }}>
                     {randomColors.map((color, index) => (
                         <div key={index} className="p-1 rounded border border-gray-300">
                             <div
                                 className="h-12 rounded-t"
                                 style={{ backgroundColor: color }}
                             ></div>
-                            <div className="text-center mt-1">
-                                <p className="font-mono text-xs text-gray-700">{color}</p> 
-                                <p className="font-mono text-xs text-gray-700">{hexToRgb(color)}</p>
+                            <div className="text-center mt-2">
+                                <p
+                                    className="font-mono text-xs text-gray-700 hover:text-black cursor-pointer mb-1"
+                                    title="Click to copy rgb code"
+                                    onClick={() => navigator.clipboard.writeText(color)}
+                                >
+                                    {color}
+                                </p>
+                                <p
+                                    className="font-mono text-xs text-gray-700 hover:text-black cursor-pointer"
+                                    title="Click to copy hex code"
+                                    onClick={() => navigator.clipboard.writeText(hexToRgb(color))}
+                                >
+                                    {hexToRgb(color)}
+                                </p>
                             </div>
                         </div>
                     ))}
