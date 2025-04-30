adc670a82b8d8ed2d489c597ae46eb5dc494c5d9
nivance <limingjian110@163.com>
2025-04-30 16:11:42 +0800

    add nav

--- src/app/[locale]/[slug]/page.tsx
+++ src/app/[locale]/[slug]/page.tsx
@@ -2,6 +2,8 @@ import SpecificColor from '@/components/specific-color';
 import { config } from "@/lib/config";
 import type { Metadata } from 'next';
 import { getTranslations } from 'next-intl/server';
+import Link from 'next/link';
+import { ArrowLeft, ChevronRight } from 'lucide-react';
 
 export const runtime = "edge";
 
@@ -31,9 +33,14 @@ export default function Page({ params }: { params: { slug: string } }) {
   const color = params.slug;
 
   return (
-    <>
+    <div className="mx-auto w-[90%] lg:w-[80%]">
+      <nav className="flex items-center text-lg font-mono text-gray-500 mt-8 border-b border-gray-200 pb-1">
+        <Link href="/" className="hover:text-blue-500">Home</Link>
+        <ChevronRight className="" size={16} />
+        <Link href={color} className="hover:text-blue-500">#{color}</Link>
+      </nav>
       <h1 className="text-2xl font-bold text-white text-center mb-4">100 Random {color} Colors</h1>
       <SpecificColor color={color} />
-    </>
+    </div>
   );
 }
