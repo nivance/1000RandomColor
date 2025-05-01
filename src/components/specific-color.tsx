4658a6b832edf8ffb7beb1e3ea5866a63a3177ac
nivance <limingjian110@163.com>
2025-05-01 13:59:12 +0800

    add exception

--- src/components/specific-color.tsx
+++ src/components/specific-color.tsx
@@ -1,16 +1,22 @@
 "use client";
 import { useEffect, useState } from 'react';
+import { useRouter } from 'next/navigation';
 import randomColor from 'randomcolor';
 import ColorCard from '@/components/color-card';
 
 export default function SpecificColor({ color }: { color: string }) {
     const [randomColors, setRandomColors] = useState<string[]>([]);
-
+    const router = useRouter(); 
+    
     useEffect(() => {
-        const colors = randomColor({ count: 100, hue: color });
-        // console.log("colors.length: " + colors.length);
-        setRandomColors(colors);
-    }, []);
+        try {
+            const colors = randomColor({ count: 100, hue: color });
+            setRandomColors(colors);
+        } catch (err) {
+            console.error("Error generating random colors:", err);
+            router.push('/404'); // 跳转到 404 页面
+        }
+    }, [color, router]);
 
     return (
         <>
