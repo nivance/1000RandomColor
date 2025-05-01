5baffd2d3c811edaae0f14b5c7499794e2e427f3
nivance <limingjian110@163.com>
2025-05-01 15:33:22 +0800

    use notFound

--- src/components/specific-color.tsx
+++ src/components/specific-color.tsx
@@ -1,22 +1,21 @@
 "use client";
 import { useEffect, useState } from 'react';
-import { useRouter } from 'next/navigation';
+import { notFound } from 'next/navigation';
 import randomColor from 'randomcolor';
 import ColorCard from '@/components/color-card';
 
 export default function SpecificColor({ color }: { color: string }) {
     const [randomColors, setRandomColors] = useState<string[]>([]);
-    const router = useRouter(); 
-    
+
     useEffect(() => {
         try {
             const colors = randomColor({ count: 100, hue: color });
             setRandomColors(colors);
         } catch (err) {
             console.error("Error generating random colors:", err);
-            router.push('/404'); // 跳转到 404 页面
+            return notFound();
         }
-    }, [color, router]);
+    }, []);
 
     return (
         <>
