357987e4aec2d4aad7b46b0e474fd46eaa0db555
nivance <limingjian110@163.com>
2025-05-04 15:27:36 +0800

    add "CSS Code Examples"

--- src/app/[locale]/[slug]/page.tsx
+++ src/app/[locale]/[slug]/page.tsx
@@ -50,15 +50,61 @@ export default function Page({ params }: { params: { slug: string } }) {
       </div>
       <SpecificColor color={color} />
       <div className="m-8">
-        <h2 className="text-2xl font-mono text-gray-500 font-bold text-center mb-4">#{color} Hex Color Code</h2>
+        <h2 className="text-2xl font-mono text-gray-500 font-bold border-b-2 text-center mb-4">#{color} Hex Color Code</h2>
         <p className="font-mono text-lg text-gray-700 dark:text-gray-300 mb-4">
           A Hex Color Code is a six-digit combination of numbers and letters used in web design and graphic design to represent colors. It begins with a hash symbol (#) followed by six characters.
-          Each pair of characters represents the intensity of red, green, and blue (RGB) in a color, using values from 00 to FF (0 to 255 in decimal). The combination of these values is used to create a range of colors that can be used in a variety of applications. 
+          Each pair of characters represents the intensity of red, green, and blue (RGB) in a color, using values from 00 to FF (0 to 255 in decimal). The combination of these values is used to create a range of colors that can be used in a variety of applications.
         </p>
         <p className="font-mono text-lg text-gray-700 dark:text-gray-300 mb-4">
-          The RGB code for the hexadecimal color code #{color} is {rgb}, in the RGB color model  is composed of {rgbPercentages[0]} red, {rgbPercentages[1]} green and {rgbPercentages[2]} blue. 
-          In the HSL color space #{color} has a hue of {hsl[0]}°(degrees), {hsl[1]} saturation and {hsl[2]} lightness. 
+          The RGB code for the hexadecimal color code #{color} is {rgb}, in the RGB color model  is composed of {rgbPercentages[0]} red, {rgbPercentages[1]} green and {rgbPercentages[2]} blue.
+          In the HSL color space #{color} has a hue of {hsl[0]}°(degrees), {hsl[1]} saturation and {hsl[2]} lightness.
         </p>
+      </div>
+      {/* CSS代码示例 */}
+      <div className="mt-8">
+        <h2 className="text-2xl font-mono text-gray-500 font-bold border-b-2 text-center mb-4">CSS Code Examples</h2>
+        <div className="mb-8">
+          <h3 className="text-xl font-mono text-gray-500 font-bold text-start mb-2">Text with hex color #{color}</h3>
+          <p className="font-mono text-lg" style={{ color: `#{color}` }}>
+            This example shows what text with the color #{color} would look like.
+          </p>
+          <pre className="bg-gray-900 text-white p-2 rounded-lg overflow-x-auto">
+            <code>{`<span style="color:#${color};">Here is the text with color: #${color}.</span>`}</code>
+          </pre>
+        </div>
+        <div className="mb-8">
+          <h3 className="text-xl font-mono text-gray-500 font-bold text-start mb-2">Border with hex color #{color}</h3>
+          <div
+            className="font-mono text-lg text-gray-700 p-2 mt-4 mb-2 border rounded"
+            style={{ border: `1px solid #${color}` }}
+          >
+            The border color of this element is #{color}。
+          </div>
+          <pre className="bg-gray-900 text-white p-2 rounded-lg overflow-x-auto">
+            <code>{`<div style="border:1px solid #${color};">Content stays here.</div>`}</code>
+          </pre>
+        </div>
+        <div className="mb-8">
+          {/* 带有背景颜色的段落 */}
+          <h3 className="text-xl font-mono text-gray-500 font-bold text-start mb-2">Background with hex color #{color}</h3>
+          <div
+            className="font-mono text-lg text-white p-2 rounded mt-4 mb-2 border rounded"
+            style={{ backgroundColor: `#${color}` }}
+          >
+            The background color of this paragraph is #{color}。
+          </div>
+          <pre className="bg-gray-900 text-white p-2 rounded-lg overflow-x-auto">
+            <code>{`<div style="background-color:#${color};">The content stays here.</div>`}</code>
+          </pre>
+        </div>
+        <div className="mb-8">
+          <h3 className="text-xl font-mono text-gray-500 font-bold text-start mb-2">CSS codes</h3>
+          <div className="bg-gray-900 text-white p-4 rounded-lg font-mono">
+            <p className="mb-2">.text {'{'} color: #{color}; {'}'}</p>
+            <p className="mb-2">.background {'{'} background-color: #{color}; {'}'}</p>
+            <p>.border {'{'} border: 1px solid #{color}; {'}'}</p>
+          </div>
+        </div>
       </div>
     </div>
   );
